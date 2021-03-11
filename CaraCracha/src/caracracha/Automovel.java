fa6e98f19ea19e7fb136d930944ff2c6961cb13e
Nágella Nasser <67282802+nagellanasser@users.noreply.github.com>
2021-03-10 22:12:56 -0300

    Add tarefa E2: Cara-Crachá
    Atividade E2 || Cara-Crachá: métodos e atributos de uma classe.

--- CaraCracha/src/caracracha/Automovel.java
+++ CaraCracha/src/caracracha/Automovel.java
@@ -50,31 +50,35 @@ public class Automovel {
     }
 
     public void acelerar() {
-        System.out.println("Vrum! Vrum!! Vrum!!!");
+        System.out.println("Vrum! Vrum!! Vrum!!!" );
     }
 
     public void freiar() {
-        System.out.println("Frenagem ativada!");
+        System.out.println("Frenagem ativada!" );
     }
     
     public void desligar() {
-        System.out.println("Ignição desativada.");
+        System.out.println("Ignição desativada." );
     }
 
     public static void main(String[] args) {
       
         Automovel auto_1 = new Automovel("GM", "Sedan", 'M', 2018, false);
+        System.out.println("Automóvel: " + auto_1.marca);
         auto_1.ligar();
-        
+                        
         Automovel auto_2 = new Automovel("Porche","Cabriolet", 'M', 2010,true);
+        System.out.println("Automóvel: " + auto_2.marca);
         auto_2.ligar();
         auto_2.acelerar();
-        
+       
         Automovel auto_3 = new Automovel("Fiat","Micro",'P', 2020, false);
+        System.out.println("Automóvel: " + auto_3.marca);
         auto_3.acelerar();
         auto_3.freiar();
         
         Automovel auto_4 = new Automovel("Ford","Campervan",'G', 1978, false);
+        System.out.println("Automóvel: " + auto_4.marca);
         auto_4.ligar();
         auto_4.acelerar();
         auto_4.freiar();
