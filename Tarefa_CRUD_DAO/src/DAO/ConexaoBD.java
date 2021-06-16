82815f247c9fd3656137cb7d0925974ff65f9a6d
Nágella Nasser <67282802+nagellanasser@users.noreply.github.com>
2021-06-15 22:26:41 -0300

    Add Tarefas: CRUD e conexão com BD

---
+++ Tarefa_CRUD_DAO/src/DAO/ConexaoBD.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Nagella
+ */
+public class ConexaoBD {
+
+    //Método responsável pela conexão com o BD
+    public static Connection Conexao() {     
+        Connection conexaoBD = null; //conexaoBD = variável de conexão
+
+        //Chamando o drive de conexão
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+        //Informações do BD
+        String url = "jdbc:mysql://localhost:<port>/<nome_bd>";
+        String usuario = "<insira_usuario";
+        String senha = "<inclua_senha_bd>";
+        
+        //Conectando com o BD
+        try {
+            Class.forName(driver); //Executa o driver
+            conexaoBD = DriverManager.getConnection(url, usuario, senha);
+            return conexaoBD;
+        } catch (Exception e) {
+            System.out.println("A conexão com o Banco de Dados falhou: " + e);
+            return null;
+        }
+    }
+}
